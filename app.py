2f33ba620fb9124be84df069e0eb3b1f8a7f3c32
MatyasGreff <matyas.greff1991@gmail.com>
2022-02-21 21:23:36 +0000

    db stuff

--- app.py
+++ app.py
@@ -1,21 +1,16 @@
 import os
-import os
-
 from flask import Flask
-"""from flask_sqlalchemy import SQLAlchemy
+from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 
-from secrets_manager import get_secret
-"""
 app = Flask(__name__)
 app.config['SECRET_KEY'] = os.environ.get('FLASK_SECRET_KEY')
-"""db_config = get_secret()
 
 app.config['SQLALCHEMY_DATABASE_URI'] = (
-    f'postgresql+psycopg2://{db_config["username"]}:' +
-    f'{db_config["password"]}@' +
-    f'{db_config["host"]}/' +
-    f'{db_config["db_name"]}'
+    f'postgresql+psycopg2://{os.getenv("POSTGRES_USER")}:' +
+    f'{os.getenv("POSTGRES_PW")}@' +
+    f'{os.getenv("POSTGRES_URL")}/' +
+    f'{os.getenv("POSTGRES_DB")}'
 )
 
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
@@ -40,17 +35,17 @@ class Transaction(db.Model):
     def __repr__(self):
         return f'{self.first_name} {self.last_name} spent {self.amount}'
 
-"""
+
 @app.route('/')
 def hello_world():
     return 'Hello world!'
 
-"""
+
 @app.route('/list_db')
 def list_db():
     transactions = Transaction.query.all()
     return '\n'.join([str(transaction) for transaction in transactions])
 
-"""
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
